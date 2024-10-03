6db1df5dc30f1b5e9eb930ddefce24b8dedee7d0
Илья <33177966+cyberquantumx@users.noreply.github.com>
2024-10-04 00:07:26 +0400

    rewrite js code and etc.

--- mishen/js/index.js
+++ mishen/js/index.js
@@ -1,124 +1,199 @@
+var TARGET_IMG = 'mishen2.png';
 
-//базовые значения
+var TEXT = {
+    COORDINATE_ERR: 'Введите координаты',
+    MISS_TARGET: 'Не попал',
+    SUCCESS_TARGET: 'Попал',
+    RADIUS_ERR: 'Не указан радиус'
+}
 
-p = [0, 10, 5, 3, 2, 1]
-rad = 5
-side = 10
-score = 0
-pos = []
+var DOT = {
+    STROKE: {
+        COLOR: 'white',
+        SIZE: 10
+    },
+    COLOR: 'black',
+    SIZE: 5
+}
 
-canvas = document.getElementById("canvas")
-ctx = canvas.getContext('2d')
-image = new Image(200,200);
+var BASE_IMAGE_SIZE = {
+    WIDTH: 200,
+    HEIGHT: 200
+}
 
-debug = false
+var SNACKBAR_TIMEOUT = 2500;
 
-function msg({t, s, to = 2500}){
+var DEBUG = true;
+
+var POINTS = {
+    MISS: 0,
+    CENTER: 10,
+    HYPERBOLA: 5,
+    RHOMB: 3,
+    CIRCLE: 2,
+    SQUARE: 1,
+}
+
+var KEY = 900;
+
+valueObj = $('#score');
+inputX = $('#x');
+inputY = $('#y');
+
+pos = {};
+
+canvas = document.getElementById("canvas");
+ctx = canvas.getContext('2d');
+image = new Image(BASE_IMAGE_SIZE.WIDTH, BASE_IMAGE_SIZE.HEIGHT);
+
+score = 0;
+radius = 5;
+
+function message(text) {
     SnackBar({
-        message: t,
-        dismissible: true,
-        status: s,
-        timeout: to
+        message: text,
+        dismissible: !DEBUG,
+        status: 'success',
+        timeout: SNACKBAR_TIMEOUT
     });
 }
 
-function r(text, status="success"){
-    msg({t: text, s: status})
+function error(text) {
+    SnackBar({
+        message: text,
+        dismissible: !DEBUG,
+        status: 'error',
+        timeout: SNACKBAR_TIMEOUT
+    });
+}
+
+function warn(text) {
+    SnackBar({
+        message: text,
+        dismissible: !DEBUG,
+        status: 'warn',
+        timeout: SNACKBAR_TIMEOUT
+    });
 }
 
 
-function update(){
-    document.getElementById('score').textContent = score;
-    debug ? console.log(score) : null;
-    document.getElementById('x').value = '';
-    document.getElementById('y').value = '';
-    document.cookie = `score=${score}`;
-}
 
-function getPos(){
-    x = document.getElementById('x').value;
-    y = document.getElementById('y').value;
-    if (x == '' || y == '' || rad== ''){ 
-       msg({t: 'Введите координаты', s: 'error'})
-    } else {
-        debug ? console.log(x,y,rad,side, score) : null
-        return [Math.abs(x),Math.abs(y), x,y]
-    }
-}
-
-function start(){
-    try {
-        _r = 0;
-        pos = getPos()
-        if(center() == true){
-            _r = 1
-        } else if(square() == true){
-            if(giper2() == true){
-                _r = 2
-            } else if(romb() == true){
-                _r = 3
-            } else if(circle() == true){
-                _r = 4
-            } else {
-                _r = 5
+const Data = {
+    get: (cookieObject) => {
+        let name = `${cookieObject}=`;
+        let cookieArray = document.cookie.split(';');
+        for (i = 0; i < cookieArray.length; i++) {
+            let cookie = cookieArray[i];
+            while (cookie.charAt(0) == ' ') {
+                cookie = cookie.substring(1);
+            }
+            if (cookie.indexOf(name) == 0) {
+                return cookie.substring(name.length, cookie.length);
             }
         }
-        score += p[_r]
-        msg({t: _r ? `+${p[_r]}` : 'не попал', s: _r ? 'success' : 'warning'})
-        image.onload = function() {
+        return false;
+    },
+
+    set: (name, value) => {
+        return document.cookie = `${name}=${value};path=/;SameSite=Strict;`;
+    },
+}
+
+function xor(str) {
+    output = '';
+    key = 320
+
+    for (var i = 0; i < str.length; ++i) {
+        output += String.fromCharCode(key ^ str.charCodeAt(i))
+    }
+
+    return output
+}
+
+function getPosition() {
+    x = inputX.val();
+    y = inputY.val();
+
+    if (!radius) {
+        error(TEXT.RADIUS_ERR);
+        return null;
+    }
+
+    if (x === '' || y === '') {
+        error(TEXT.COORDINATE_ERR);
+        return null;
+    }
+
+    return ({
+        x: Number(x),
+        y: Number(y),
+        absX: Math.abs(x),
+        absY: Math.abs(y)
+    })
+}
+
+function drawTarget() {
+    if (image) {
+        image.onload = () => {
             canvas.width = (innerHeight - 8) / 3;
             canvas.height = (innerHeight - 8) / 3;
-            _scale = Math.min( canvas.width /image.naturalWidth , canvas.height / image.naturalHeight );
-            _x = (canvas.width / 2 - (image.naturalWidth / 2) * _scale) | 0;
-            _y = (canvas.height / 2 - (image.naturalHeight / 2) * _scale) | 0;
-            canvas.getContext("2d").drawImage(image,_x,0,image.naturalWidth * _scale, image.naturalHeight * _scale);
-            __x = ((canvas.width/2)-7) + (canvas.width*(0.048*x))
-            __y = ((canvas.height/2)-4) - (canvas.height*(0.048*y))
-            ctx.fillStyle = "white";
-            ctx.fillRect(__x, __y, 10,10);
-            ctx.fillStyle = "black";
-            ctx.fillRect(__x+2.5, __y+2.5, 5,5);
+            scaleImg = Math.min(canvas.width / image.naturalWidth,
+                canvas.height / image.naturalHeight);
+            imgX = (canvas.width / 2 - (image.naturalWidth / 2) * scaleImg) | 0;
+
+            imgWidth = image.naturalWidth * scaleImg;
+            imgHeight = image.naturalHeight * scaleImg;
+
+            ctx.drawImage(image, imgX, 0, imgWidth, imgHeight);
         }
-        image.src = 'mishen2.png';
-        update();
-        
-    } catch (e) {
-        debug ? msg({t: String(e.message + e.stack), s: 'error', to: false}) : null;
+        image.src = TARGET_IMG;
+        return true;
     }
 }
 
-function square(){
-    return ((pos[0] <= side) && (pos[1]) <= side)
+function drawHit(x, y) {
+    dotX = ((canvas.width / 2) - 7) + (canvas.width * (0.048 * x));
+    dotY = ((canvas.height / 2) - 4) - (canvas.height * (0.048 * y));
+    ctx.fillStyle = DOT.STROKE.COLOR;
+    ctx.fillRect(dotX, dotY, DOT.STROKE.SIZE, DOT.STROKE.SIZE);
+    ctx.fillStyle = DOT.COLOR;
+    ctx.fillRect(dotX + DOT.SIZE / 2, dotY + DOT.SIZE / 2, DOT.SIZE, DOT.SIZE);
 }
 
-function center(){
-    return pos[0] == 0 && pos[1] == 0
+$(document).ready(() => {
+    score = Number(xor(Data.get('score')))
+    valueObj.text(score);
+    console.log(score)
+    drawTarget();
+})
+
+function check() {
+    pos = getPosition();
+    if (pos == null) return;
+
+    result = (pos.x === 0 && pos.y === 0) ? POINTS.CENTER : 0 || //центр
+        (1 / pos.absX >= pos.absY) ? POINTS.HYPERBOLA : 0 || //гипербола
+        ((pos.absX + pos.absY) / 2 <= radius) ? POINTS.RHOMB : 0 || //ромб
+        ((pos.absX ** 2 + pos.absY ** 2) <= (radius * 2) ** 2) ? POINTS.CIRCLE : 0 || //круг
+        ((pos.absX <= radius * 2) && (pos.absY) <= (radius * 2)) ? POINTS.SQUARE : 0; //квадрат
+
+    score += result;
+    DEBUG ? console.log(score, pos) : null;
+
+    if (result != 0) message(`${TEXT.SUCCESS_TARGET} +${result}`)
+    else warn(TEXT.MISS_TARGET);
+
+    drawHit(pos.x, pos.y);
+
+    inputX.val('');
+    inputY.val('');
+
+    Data.set('score', xor(score.toString()));
+    valueObj.text(score);
+
 }
 
-function circle(){
-    pos = getPos()
-    return ((pos[0]**2 + pos[1]**2) <= side**2)
-}
-
-function giper2(){
-    return (1/pos[0] >= pos[1])
-}
-
-function romb(){
-    return ((pos[0]+pos[1])/2<=side/2)
-}
-
-window.onload = function() {
-    cookie = document.cookie
-    if(cookie){
-        data = cookie.split('=')
-        console.log(data)
-        score = Number(data[1])
-        update()
-    }
-}
-
-function clearCookie(){
-    document.cookie = 'score=0';
+function clearData() {
+    document.cookie = 'score=Ű';
     window.location.reload();
 }

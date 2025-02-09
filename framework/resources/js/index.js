e0f85b9bb23bab4401eef3784d3b6b18dbc070c5
Илья <33177966+cyberquantumx@users.noreply.github.com>
2025-02-10 00:09:19 +0400

    123

---
+++ framework/resources/js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    AOS.init();
+    $('.heart').click(function() {
+        window.open('https://www.tbank.ru/cf/5yf7xr6MoaH', '_blank');
+    });
+    $('#tgBtn').click(function() {
+        window.open('https://t.me/cyberboyx', '_blank');
+    });
+});
